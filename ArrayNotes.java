8d5b1e36b693c31b3a0eecf35a17c34ab32ecee5
Matt Callaghan <mcallaghan@naperville203.org>
2021-11-30 13:15:02 -0600

    finished notes on arrays of references

--- ArrayNotes.java
+++ ArrayNotes.java
@@ -119,6 +119,58 @@ public class ArrayNotes
         }
     }
     
+    public static void createArrayOfCalendars()
+    {
+        /*
+         * Create an array of 12 calendars, each calendar initialized to the start of the month.
+         * 
+         * When we create an array of objects, each element is initialized to null.  We have to 
+         *      explicitly create new objects and assign to each element.
+         */
+        
+        GregorianCalendar[] calendars = new GregorianCalendar[12];
+        
+        /*
+         * At this point every element in teh array is null.
+         */
+        
+        for(GregorianCalendar calendar : calendars)
+        {
+            System.out.println(calendar);
+        }
+        
+        /*
+         * Create a new calendar object and assign it to each element in the array
+         */
+        for(int i = 0; i < calendars.length; i++)
+        {
+            calendars[i] = new GregorianCalendar(2021, i + 1, 1);
+        
+        }
+        
+        for(GregorianCalendar calendar : calendars)
+        {
+            System.out.println(calendar);
+        }
+        
+        /*
+         * An enhanced for loop cannot modify the values of the elements in the array (e.g., 
+         *      references to calendars), but we can call mutator methods which modify the
+         *      properties of the referenced objects (e.g., day of the month).
+         */
+        
+        for (GregorianCalendar calendar : calendars )
+        {
+            calendar.add(GregorianCalendar.DAY_OF_MONTH, 2);
+        
+        }
+        
+        for(GregorianCalendar calendar : calendars)
+        {
+            System.out.println(calendar);
+        }
+        
+    }
     
     
     
